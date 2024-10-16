aeafaa3ac3d4eeb72510746b209019564991a1c0
keanecodes <keanecodes@gmail.com>
2024-10-16 20:51:39 +0000

    #1 Convert to offline-available PWA & Use styled-components with `npm i -D @vitejs/plugin-react vite-plugin-pwa sass` and `npm i styled-components`

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
+
+import { registerSW } from "virtual:pwa-register";
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
