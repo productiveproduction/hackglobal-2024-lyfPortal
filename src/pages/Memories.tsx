c974ebe9324f20bdc9eb22b2575016316eb0d418
keanecodes <keanecodes@gmail.com>
2024-10-28 19:38:16 +0800

    alamak

--- src/pages/Memories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Memories() {
-  return (
-    <iframe src="https://geomemory.vercel.app/" style={{width:"100dvw", height:"calc(100dvh - 4rem)"}}/>
-  )
-}
